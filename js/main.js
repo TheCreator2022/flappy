c17bb72ea6d57fb2fd6fddd1d77e986310e28dae
TheCreator2022 <106817598+TheCreator2022@users.noreply.github.com>
2022-07-28 12:53:30 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 var score = 0
 var x = [960, 420, 60]
 var y = [15, 45, 25]
+let GetLeft = () => {
+  x[1] -= 10
+  FlappyClicked();
+}
+const FlappyClicked = () => {
+  document.getElementById("audio1").play();
+}
 
 function UpdateGameArea() {
   document.getElementById("score").style.left = x[0] + "px"
   document.getElementById("score").style.top = y[0] + "px"
+  document.getElementById("player").style.left = x[1] + "px"
+  document.getElementById("player").style.top = y[1] + "px"
 }
 
 setInterval(UpdateGameArea, 100)
